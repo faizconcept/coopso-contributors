9809b2d957839e92d24d2b6834ff845a26ab5217
faizcoopso <faiz.coopso@gmail.com>
2022-10-20 19:23:30 +0530

    update the plugin description

--- coopso-contributors.php
+++ coopso-contributors.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Coopso Contributors
  * Plugin URI: http://wordpress.org/plugins/coopso-contributors/
- * Description: This is a WordPress contributors plugin. Admin can select the multiple users who contribute into the post. When admin can activate this plugin then contributors' meta box will be displayed and admin can select the authors who contribute to the post and those name will be displayed the at the front end under the post title with Gravatars of the authors.
+ * Description: This is a WordPress contributors plugin. Admin can select the multiple users who contribute to the post. When the admin can activate this plugin then the contributors' meta box will be displayed and the admin can select the users (editors, authors, admin) who can contribute to the post. At the front end under the post details page after the post content, there will be contributors box will be displayed. In the contributor's box,   users (authors, editors, and admin) names and Gravatars will be displayed. The guest users can click on the Gravatar or username and it will be redirected to the author page.
  * Version: 1.0.0
  * Author: Faiz A Shaikh
  * Author URI: https://profiles.wordpress.org/faiz786/
