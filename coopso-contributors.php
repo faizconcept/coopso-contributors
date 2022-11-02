3f944fd30aeca228714541523328842781977180
faizcoopso <faiz.coopso@gmail.com>
2022-11-02 16:25:15 +0530

    add the licence info

--- coopso-contributors.php
+++ coopso-contributors.php
@@ -6,6 +6,18 @@
  * Version: 1.0.0
  * Author: Faiz A Shaikh
  * Author URI: https://profiles.wordpress.org/faiz786/
+ * Coopso Contributors is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Coopso Contributors is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Coopso Contributors. If not, see <https://www.gnu.org/licenses/>.
  *
  * @package Contributors
  */
